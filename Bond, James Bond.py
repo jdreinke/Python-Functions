bb199a31c03233b57d079429f964b95167a7d7a1
Justin Reinke <92952926+jdreinke@users.noreply.github.com>
2021-10-21 15:05:55 -0600

    Add files via upload

---
+++ Bond, James Bond.py
@@ -0,0 +1,4 @@
+def introduction(first_name, last_name):
+  return last_name + ', ' + first_name + ' ' + last_name
+# Enter your first and last name between the quotations to whatever you like
+print(introduction('first_name', 'last_name'))
