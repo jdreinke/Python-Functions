95012322ed8aeaf176db9951d3db9b4c40fa57fa
Justin Reinke <92952926+jdreinke@users.noreply.github.com>
2021-10-21 14:48:35 -0600

    Add files via upload

---
+++ Tip Calculator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def tip(total, percentage):
+  return total * (percentage / 100)
+# Add the total bill amount and the percentage you want (18% = 18)
+print(tip(total, percentage))
